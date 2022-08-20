0451cd64032587227929ebca9c16217486f7087c
python1111111111111 <58154286+python1111111111111@users.noreply.github.com>
2022-08-20 11:31:38 -0500

    upgraded to v3

--- PoloLib.lua
+++ PoloLib.lua
@@ -1,13 +1,34 @@
 local library = {}
 
-function library:CreateWindow(wname, wfont = Enum.Font.SourceSansLight) 
+function library:CreateWindow(wname) 
     local ScreenGui = Instance.new("ScreenGui")
+    ScreenGui.Name = "SCRIPT_HUB_MOSS"
     local MainGui = Instance.new("TextLabel")
     local Frame = Instance.new("Frame")
-
+	wfont = Enum.Font.SciFi
     --Properties:
+	local X = Instance.new("TextButton")
 
-    ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+	--Properties:
+	
+	X.Parent = Frame
+	X.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+	X.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	X.BorderSizePixel = 0
+	X.Position = UDim2.new(0.902438939, 0, -0.102661595, 0)
+	X.Size = UDim2.new(0, 40, 0, 26)
+	X.Font = wfont
+	X.Text = "X"
+	X.TextColor3 = Color3.fromRGB(255, 255, 255)
+	X.TextScaled = true
+	X.TextSize = 14.000
+	X.TextWrapped = true
+	X.ZIndex = 3
+	
+	X.MouseButton1Down:Connect(function()
+	ScreenGui:Destroy()
+	end)
+    ScreenGui.Parent = game.CoreGui
 
     MainGui.Name = "MainGui"
     MainGui.Parent = ScreenGui
@@ -21,6 +42,7 @@ function library:CreateWindow(wname, wfont = Enum.Font.SourceSansLight)
     MainGui.TextSize = 14.000
     MainGui.TextWrapped = true
     MainGui.Text = wname
+    MainGui.ZIndex = 2
     Frame.Parent = MainGui
     Frame.BackgroundColor3 = Color3.fromRGB(27, 27, 27)
     Frame.BorderColor3 = Color3.fromRGB(27, 27, 27)
@@ -34,23 +56,61 @@ function library:CreateWindow(wname, wfont = Enum.Font.SourceSansLight)
         script.Parent.Draggable = true
     end
     coroutine.wrap(MKHDTR_fake_script)()
-    
+    inum = -0.05
     local PoloLib = {}
-
-    function PoloLib:CreateButton(bname)
-        TextButton.Parent = Frame
-        TextButton.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+	local sections = {}
+    function PoloLib:CreateButton(bname, sec, func)
+    	inum += 0.05
+        local TextButton = Instance.new("TextButton")
+        TextButton.Parent = sec
+        TextButton.BackgroundColor3 = Color3.fromRGB(27, 27, 27)
         TextButton.BorderColor3 = Color3.fromRGB(40, 40, 40)
-        TextButton.Position = UDim2.new(0.0268292688, 0, 0.0304182507, 0)
-        TextButton.Size = UDim2.new(0, 123, 0, 21)
+        TextButton.Position = UDim2.new(0.03, 0, 0.0304182507+inum, 0)
+        TextButton.Size = UDim2.new(0, 280, 0, 22)
         TextButton.Font = wfont
         TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
         TextButton.TextSize = 14.000
         TextButton.Text = bname
+        TextButton.MouseButton1Down:Connect(func)
     end
+    function ClearB()
+    inum = -0.05
+    end
+    local inum2 = -0.09
+    function PoloLib:CreateSection(SectionName)
+    	ClearB()
+        local TextButton = Instance.new("TextButton")
+        local ScrollingFrame = Instance.new("ScrollingFrame")
+		inum2 += 0.09
+		table.insert(sections, ScrollingFrame)
+        TextButton.Parent = Frame
+        TextButton.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+        TextButton.BorderColor3 = Color3.fromRGB(40, 40, 40)
+        TextButton.Position = UDim2.new(0.000362265971/2, 0, -0.00186580757+inum2, 0)
+        TextButton.Size = UDim2.new(0, 98, 0, 22)
+        TextButton.Font = wfont
+        TextButton.Text = SectionName
+        TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+        TextButton.TextSize = 14.000
+		TextButton.MouseButton1Down:Connect(function()
+		for i,v in pairs(sections) do
+		v.Visible = false
+		end
+		ScrollingFrame.Visible = true
+
+		end)
+        ScrollingFrame.Parent = Frame
+        ScrollingFrame.Active = true
+        ScrollingFrame.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+        ScrollingFrame.BorderColor3 = Color3.fromRGB(40, 40, 40)
+        ScrollingFrame.Position = UDim2.new(0.243902445, 0, -0.0261500254, 0)
+        ScrollingFrame.Size = UDim2.new(0, 309, 0, 269)
+        ScrollingFrame.ZIndex = 1
+		for i,v in pairs(sections) do
+		v.Visible = false
+		end
+        return ScrollingFrame
+    end
+    
     return PoloLib
 end
-
-return library
--- Scripts:
-
